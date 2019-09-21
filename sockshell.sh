41e84e2769aca0a132b3b1c2267354df10cb3a77
NialeChen <793530075@qq.com>
2019-09-21 12:42:37 +0800

    Update sockshell.sh

--- sockshell.sh
+++ sockshell.sh
@@ -12,7 +12,7 @@ sudo apt-get install -y python-pip
 sudo python2.7 get-pip.py
 sudo pip install shadowsocks
 sudo echo "{" > /etc/shadowsocks.json
-sudo echo "\"server\":\"140.82.55.173\"," >> /etc/shadowsocks.json
+sudo echo "\"server\":\"144.202.63.179\"," >> /etc/shadowsocks.json
 sudo echo "\"local_address\":\"127.0.0.1\"," >> /etc/shadowsocks.json
 sudo echo "\"local_port\":1080," >> /etc/shadowsocks.json
 sudo echo "\"port_password\": {" >> /etc/shadowsocks.json
